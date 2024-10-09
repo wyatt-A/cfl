48b57368d6e0e9659b14965f1cfb176a284536d3
wyatt <waustin47@outlook.com>
2024-10-08 21:20:04 -0400

    update

--- src/lib.rs
+++ src/lib.rs
@@ -178,7 +178,6 @@ impl CflWriter {
             self.write_op(*dst_idx,*value,&op).unwrap()
         });
         self.mmap.flush().map_err(|_|CflError::MmapFlush)?;
-        err
         Ok(())
     }
 
