b1912f0c5e6b1cba4218a77bd0b3a0dc873b2eef
wyatt-A <waustin47@outlook.com>
2025-01-18 13:15:31 -0500

    added linalg-mkl system feature

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,7 @@ use std::path::PathBuf;
 pub use num_complex;
 pub use ndarray;
 pub use ndarray_stats;
-#[cfg(any(feature = "linalg-openblas", feature = "linalg-netlib"))]
+#[cfg(any(feature = "linalg-openblas", feature = "linalg-netlib", feature = "linalg-mkl"))]
 pub use ndarray_linalg;
 
 #[derive(Debug)]
